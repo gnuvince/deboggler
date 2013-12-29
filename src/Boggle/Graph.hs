9c6c39a95f51524556584fec17f666478b1c4a1e
Vincent Foley <vfoley@gmail.com>
2013-12-28 22:52:11 -0500

    Added haddock comments to graph module and simplified implementation of dfs.

--- src/Boggle/Graph.hs
+++ src/Boggle/Graph.hs
@@ -16,18 +16,25 @@ type Cube = Int
 newtype Graph = MkGraph (M.IntMap (S.Set Cube))
     deriving (Show, Eq)
 
+-- |Create an empty graph.
 empty :: Graph
 empty = MkGraph (M.empty)
 
+-- |Return the nodes adjacent to a given cube in the graph.
+-- If the node doesn't exist, return the empty set.
 neighbors :: Cube -> Graph -> S.Set Cube
 neighbors node (MkGraph m) =
     M.findWithDefault S.empty node m
 
+-- |Add an edge between two cubes in the graph.
+-- Because the graph is undirected, add an edge
+-- from @x@ to @y@ and from @y@ to @x@.
 addEdge :: Cube -> Cube -> Graph -> Graph
 addEdge x y (MkGraph m) =
     MkGraph $ M.insertWith S.union x (S.singleton y)
             $ M.insertWith S.union y (S.singleton x) m
 
+-- |Return a graph for a boggle board of size @NxN@.
 boggleGraph :: Int -> Graph
 boggleGraph size = L.foldl' addCube empty [0 .. size^2 - 1]
     where
@@ -49,10 +56,13 @@ boggleGraph size = L.foldl' addCube empty [0 .. size^2 - 1]
         from2D :: (Int, Int) -> Int
         from2D (i, j) = size*i + j
 
-dfs :: Int -> Int -> Graph -> [[Cube]]
+-- |Perform a depth-first search of the graph starting at a
+-- given cube for a given depth. Return a list of paths.
+dfs :: Int -> Cube -> Graph -> [[Cube]]
 dfs depth cube g = go depth cube S.empty
     where go 0 _ _    = []
           go 1 curr _ = [[curr]]
-          go d curr visited = map (curr:) $ concat [go (d-1) adj (S.insert curr visited)
-                                                    | adj <- S.toList (neighbors curr g)
-                                                    , adj `S.notMember` visited]
+          go d curr visited = [curr:path
+                               | adj <- S.toList (neighbors curr g)
+                               , adj `S.notMember` visited
+                               , path <- go (d-1) adj (S.insert curr visited)]
